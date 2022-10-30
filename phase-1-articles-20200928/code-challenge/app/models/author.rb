979361b163034b3cbdad884ddaef6447e4216f92
David <david.njoroge@student.moringaschool.com>
2022-10-30 04:15:12 +0300

    completed author.rb deliverables

--- phase-1-articles-20200928/code-challenge/app/models/author.rb
+++ phase-1-articles-20200928/code-challenge/app/models/author.rb
@@ -1,11 +1,31 @@
-class Author
-  attr_accessor :name
+class Author 
+  attr_reader :name
 
-
-  def initialize(name)
+  def initialize (name)
     @name = name
-  
+  end
+ 
+  def articles
+    Article.all.select do |article|
+      article.author.name == self.name
+    end
+  end
+ 
+  def magazines
+    self.articles.map do |article|
+      article.magazine
+    end.uniq
   end
 
-
+   def add_article(title, magazine_name, magazine_category)
+    Article.new(title, self, magazine_name, magazine_category)
+  end
+   
+  def topic_areas
+    self.magazines.map do |magazine|
+      magazine.category
+    end.uniq
+  end
 end
+ 
+
