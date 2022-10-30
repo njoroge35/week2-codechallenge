f035a91231b9263101ce0ebe8d6d83fc7ef1593a
njoroge35 <110379615+njoroge35@users.noreply.github.com>
2022-10-30 19:46:41 +0300

    completed articles

--- phase-1-articles-20200928/code-challenge/app/models/article.rb
+++ phase-1-articles-20200928/code-challenge/app/models/article.rb
@@ -4,14 +4,14 @@ class Article
 
         attr_reader :title, :author, :magazine
         @@all = []
-        # initialize with title , author and magazine
+       
         def initialize(title, author, magazine_name , magazine_category)
             @title = title
             @author = Author.new(author)
             @magazine = Magazine.new(magazine_name, magazine_category)
             @@all << self
         end
-        # class method that returns all articles
+      
         def self.all
             @@all
         end
