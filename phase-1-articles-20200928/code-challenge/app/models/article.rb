21a223a7fe1d173062f20b8734c8a0f0e36c3003
David <david.njoroge@student.moringaschool.com>
2022-10-30 10:46:03 +0300

    completed articles

--- phase-1-articles-20200928/code-challenge/app/models/article.rb
+++ phase-1-articles-20200928/code-challenge/app/models/article.rb
@@ -1,4 +1,22 @@
+require_relative "./author.rb"
+require_relative "./magazine.rb"
 class Article
 
+        attr_reader :title, :author, :magazine
+        @@all = []
+        # initialize with title , author and magazine
+        def initialize(title, author, magazine_name , magazine_category)
+            @title = title
+            @author = Author.new(author)
+            @magazine = Magazine.new(magazine_name, magazine_category)
+            @@all << self
+        end
+        # class method that returns all articles
+        def self.all
+            @@all
+        end
+    
+    
+    end
 
-end
+
--- phase-1-articles-20200928/code-challenge/app/models/magazine.rb
+++ phase-1-articles-20200928/code-challenge/app/models/magazine.rb
@@ -1,12 +1,39 @@
-class Magazine
-  attr_accessor :name, :category
-
-
-  def initialize(name, category)
+require_relative "./article"
+class Magazine 
+  attr_accessor :name , :category
+  @@all = []
+  def initialize(name , category)
     @name = name
     @category = category
-
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def contributors
+    Article.all.select do |article|
+      article.magazine.name == self.name && article.magazine.category == self.category
+    end.map do |article|
+      article.author
+    end
+  end
+ 
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name
+    end
   end
 
+    Article.all.select do |article|
+      article.magazine.name == self.name && article.magazine.category == self.category
+    end.map do |article|
+      article.title
+    end
+  end
 
-end
+  def contributing_authors
+    self.contributors.filter {|author|author.articles.count > 2}
+  end 
+   
