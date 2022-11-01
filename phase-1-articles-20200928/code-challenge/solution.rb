7784af3498f7a0a8182965b7c4efd5b3babf1407
David <david.njoroge@student.moringaschool.com>
2022-11-01 11:19:02 +0300

    updated solution

--- phase-1-articles-20200928/code-challenge/solution.rb
+++ phase-1-articles-20200928/code-challenge/solution.rb
@@ -1,1 +1,93 @@
 # Please copy/paste all three classes into this file to submit your solution!
+
+class Author
+    attr_reader :name
+  
+  @@all= []
+    def initialize(name)
+      @name = name
+    @@all<< self
+    end
+  
+  def name
+    @name
+  end
+  
+  def articles
+    Article.all.filter  {|article| article.author == self}
+  end
+  
+    def magazines
+  self.articles.map{|article| article.magazine}.uniq
+  end
+  
+  def add_article(magazine, title)
+  
+  Article.new( self, magazine, title)
+  end
+  
+  def topic_areas
+    self.magazines.map{|magazine|magazine.category}.uniq
+  end
+  
+  def self.all
+    @@all
+  end
+  end
+  
+  class Magazine
+    attr_accessor :name, :category
+  
+  @@all = []
+    def initialize(name, category)
+      @name = name
+      @category = category
+      @@all << self
+    end
+  
+  def name
+    @name
+  end
+  
+  def category
+    @category
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def articles
+    Article.all.filter {|article| article.magazine == self}
+  end
+  
+  def contributors
+   self.articles.map {|article|article.author}.uniq
+  end
+  
+    def self.find_by_name(name:)
+   self.all.find{|magazine| magazine.name == name}
+    end
+
+    def article_titles
+    self.articles.map {|article| article.title}
+    end
+  def contributing_authors
+      self.contributors.filter {|author| author.articles.count > 2}
+    end
+end
+  
+  class Article
+    attr_reader :author, :magazine, :title
+    
+    @@all = []
+    def initialize(author:, magazine:, title:)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    
+    def self.all
+            @@all
+        end
+     end
--- phase-1-articles-20200928/code-challenge/tools/console.rb
+++ phase-1-articles-20200928/code-challenge/tools/console.rb
@@ -6,6 +6,15 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+def reload
+  load 'config/environment.rb'
+end
+magazine1 = Magazine.new("magazine1", "category1")
+magazine2 = Magazine.new("magazine2", "category2")
+art1 = Article.new("article1", "author1", "magazine1", "category1")
+art2 = Article.new("article2", "author2", "magazine2", "category2")
+author1 = Author.new("author1")
+author2 = Author.new("author2")
 
 
 
